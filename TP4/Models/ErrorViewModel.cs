99f6b016875eb991d9a885e8f60ebea171dc8f6b
BR0K1NG <gamerwepx19@gmail.com>
2025-05-09 23:16:36 +0100

    removed TP4 from TP5 branch

--- TP4/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WL_TP4.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
